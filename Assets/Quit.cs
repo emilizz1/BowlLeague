972bd20d3304de234a302f8c37e7180566c7b747
Emilijus <slibinas2012@gmail.com>
2018-12-01 14:09:52 +0200

    Added quit option.

---
+++ Assets/Quit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+	void Awake ()
+    {
+        int gameInstances = FindObjectsOfType<Quit>().Length;
+        if(gameInstances > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+	}
+}
